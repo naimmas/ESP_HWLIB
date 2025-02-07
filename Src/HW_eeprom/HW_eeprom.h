2b0a75d34c8f14007a9f53999f59e64ff435e64d
Naim <naeemmas@gmail.com>
2025-02-07 22:18:18 +0300

    refactor(hw-eeprom): update function names
    add comments

--- Src/HW_eeprom/HW_eeprom.h
+++ Src/HW_eeprom/HW_eeprom.h
@@ -37,13 +37,34 @@ typedef enum data_validity_t_enum
 * External function declarations.
 ***************************************************************************************************/
 
-extern bool eeprom_init(uint32_t p_data_size);
-extern data_validity_t eeprom_read_data(uint8_t * p_ptr_data_buffer, 
+/**
+ * @brief This function initializes the EEPROM module.
+ * 
+ * @param p_data_size input: The size of the data to be stored in the EEPROM in bytes
+ * @retval true if the EEPROM is initialized successfully
+ * @retval false if the EEPROM is not initialized successfully
+ */
+extern bool ardal_eeprom_init(uint32_t p_data_size);
+
+/**
+ * @brief This function deinitializes the EEPROM module.
+ */
+extern data_validity_t ardal_eeprom_read_data(uint8_t * p_ptr_data_buffer, 
                                         uint32_t p_data_size,
                                         uint32_t p_starting_address);
-extern bool eeprom_write_data(uint8_t * p_ptr_data_buffer, 
+
+/**
+ * @brief This function writes data to the EEPROM, and calculates the checksum.
+ * 
+ * @param p_ptr_data_buffer input: Pointer to the data buffer
+ * @param p_data_size input: The size of the data to be stored in the EEPROM in bytes
+ * @param p_starting_address input: The starting address of the data in the EEPROM
+ * @return true 
+ * @return false 
+ */
+extern bool ardal_eeprom_write_data(uint8_t * p_ptr_data_buffer, 
                               uint32_t p_data_size,
                               uint32_t p_starting_address);
-extern uint32_t eeprom_get_data_start_adrs(void);
+extern uint32_t ardal_eeprom_get_data_start_adrs(void);
 
 #endif /* HW_EEPROM_H */
