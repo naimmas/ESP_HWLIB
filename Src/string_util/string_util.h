2e309f2082a4c521a8884f6d3c5e881d94f13de5
Naim <naeemmas@gmail.com>
2025-02-07 22:11:12 +0300

    refactor(string_util): add comments to header file

--- Src/string_util/string_util.h
+++ Src/string_util/string_util.h
@@ -29,8 +29,33 @@
 * External function declarations.
 ***************************************************************************************************/
 
+/**
+ * @brief This function reverses the input string.
+ * 
+ * @param p_ptr_str input: pointer to the input string.
+ * @param p_str_len input: length of the input string.
+ */
 void string_reverse(char * p_ptr_str, uint32_t p_str_len);
-uint32_t string_itoa(int32_t p_num, char * p_ptr_str, uint32_t p_digit, uint32_t p_base);
+
+/**
+ * @brief This function converts an integer number to a string.
+ * 
+ * @param p_num input: The number to be converted.
+ * @param p_ptr_str output: The string that will hold the converted number.
+ * @param p_digit input: The number of digits in the converted number.
+ * @param p_base input: The base of the converted number, 10 or 16.
+ * @return The length of the converted string.
+ */
+uint32_t string_itoa(int32_t p_num, char * p_ptr_str, uint32_t p_digit, base_t p_base);
+
+/**
+ * @brief This function converts a float number to a string.
+ * 
+ * @param p_fnum input: the float number to be converted.
+ * @param p_ptr_str output: the string that will hold the converted number.
+ * @param p_after_point input: the number of digits after the point.
+ * @retval The length of the converted string.
+ */
 uint32_t string_ftoa(float p_fnum, char * p_ptr_str, uint32_t p_after_point);
 
 #endif /* STRING_UTIL_H */
